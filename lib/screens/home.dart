b3ae3bb4b585a06450b2b7e406554655c9ecc83f
salahkamal <salahkamalpro96@gmail.com>
2020-06-11 10:16:04 +0200

    reformat home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,10 +13,12 @@ class Home extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: Center(
               child: SingleChildScrollView(
-                              child: Column(
-
-            children: <Widget>[
-                Text('Current Speed',style: TextStyle(fontSize: 50),),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Current Speed',
+                  style: TextStyle(fontSize: 50),
+                ),
                 (mySpeed != null)
                     ? Text(
                         mySpeed,
@@ -27,36 +29,55 @@ class Home extends StatelessWidget {
                         ),
                       )
                     : CircularProgressIndicator(),
-                    Text('Km/h',style: TextStyle(fontSize: 30),),
-                    SizedBox(height: 50,),
-                    Text('From 10 to 30',style: TextStyle(fontSize: 40),),
-                    Consumer<LocationService>(builder: (_,loca,__){
-                     return Text(
-                        (loca.accTime != null) ? loca.accTime.toString(): '--',
-                        style: TextStyle(
-                          fontFamily: 'Digital1',
-                          fontSize: 80,
-                          color: Colors.greenAccent[400],
-                        ),
-                      );
-                    }),
-                    Text('Seconds',style: TextStyle(fontSize: 30),),
-                    SizedBox(height: 50,),
-                    Text('From 30 to 10',style: TextStyle(fontSize: 40),),
-                    Consumer<LocationService>(builder: (_,loca,__){
-                     return Text(
-                        (loca.decTime != null) ? loca.decTime.toString(): '--',
-                        style: TextStyle(
-                          fontFamily: 'Digital1',
-                          fontSize: 80,
-                          color: Colors.greenAccent[400],
-                        ),
-                      );
-                    }),
-                    Text('Seconds',style: TextStyle(fontSize: 30),),
-            ],
-          ),
-              )),
+                Text(
+                  'Km/h',
+                  style: TextStyle(fontSize: 30),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'From 10 to 30',
+                  style: TextStyle(fontSize: 40),
+                ),
+                Consumer<LocationService>(builder: (_, loca, __) {
+                  return Text(
+                    (loca.accTime != null) ? loca.accTime.toString() : '--',
+                    style: TextStyle(
+                      fontFamily: 'Digital1',
+                      fontSize: 80,
+                      color: Colors.greenAccent[400],
+                    ),
+                  );
+                }),
+                Text(
+                  'Seconds',
+                  style: TextStyle(fontSize: 30),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'From 30 to 10',
+                  style: TextStyle(fontSize: 40),
+                ),
+                Consumer<LocationService>(builder: (_, loca, __) {
+                  return Text(
+                    (loca.decTime != null) ? loca.decTime.toString() : '--',
+                    style: TextStyle(
+                      fontFamily: 'Digital1',
+                      fontSize: 80,
+                      color: Colors.greenAccent[400],
+                    ),
+                  );
+                }),
+                Text(
+                  'Seconds',
+                  style: TextStyle(fontSize: 30),
+                ),
+              ],
+            ),
+          )),
         ),
       ),
     );
